fe8c6cd6fc7aa322410a1ee7288a3c95c3c2b4c6
vanotis720 <vanotis720@gmail.com>
2023-02-24 11:57:42 +0200

    ui list of todos and input

--- App.js
+++ App.js
@@ -1,45 +1,51 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { AntDesign, Entypo } from '@expo/vector-icons';
+import { View, Text, StyleSheet, FlatList, TextInput } from 'react-native';
+import { AntDesign, MaterialIcons, Entypo } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const todos = [
+const TODOS = [
 	{
 		id: 1,
 		title: 'manger le soir',
 		time: '07:30',
 		location: 'my home',
-		frequency: 'daily'
+		frequency: 'daily',
+		isFinished: true
 	},
 	{
 		id: 2,
 		title: 'coucher le bebe',
 		time: '22:00',
 		location: 'my home',
-		frequency: 'daily'
+		frequency: 'daily',
+		isFinished: false
 	},
 	{
 		id: 3,
 		title: 'manger le matin',
 		time: '11:25',
 		location: 'gym',
-		frequency: 'weekly'
+		frequency: 'weekly',
+		isFinished: true
 	},
 	{
 		id: 4,
 		title: 'Designer affiche',
 		time: '11:25',
 		location: 'gym',
-		frequency: 'monthly'
+		frequency: 'monthly',
+		isFinished: false
 	},
 	{
 		id: 5,
 		title: 'jouer',
 		time: '11:25',
 		location: 'restaurant',
-		frequency: 'daily'
+		frequency: 'daily',
+		isFinished: false
 	},
 ];
+
 
 export default function App() {
 	return (
@@ -55,23 +61,35 @@ export default function App() {
 			</View>
 			<View style={styles.bottomSection}>
 				<FlatList
-					data={todos}
-					renderItem={(todo) => {
-						<View>
-							<AntDesign name="checkcircle" size={24} color="black" />
-							<View>
-								<Text>kbfskbgk</Text>
-								<View>
-									<Text>kbfskbgk</Text>
-									<Entypo name="dot-single" size={24} color="black" />
-									<Text>kbfskbgk</Text>
-									<Entypo name="dot-single" size={24} color="black" />
-									<Text>kbfskbgk</Text>
+					data={TODOS}
+					renderItem={({ item }) =>
+						<View style={styles.itemContainer}>
+							{
+								(item.isFinished) ? (
+									<AntDesign name="checkcircle" size={40} color="green" />
+								) : (
+									<MaterialIcons name="radio-button-unchecked" size={50} color="gray" />
+								)
+							}
+							<View style={styles.itemContainerContent}>
+								<Text style={styles.itemContainerContentTitle}>{item.title}</Text>
+								<View style={styles.itemContainerContentMetadata}>
+									<Text style={styles.metadata}>{item.time}</Text>
+									<Entypo name="dot-single" size={24} color="gray" />
+									<Text style={styles.metadata}>{item.location}</Text>
+									<Entypo name="dot-single" size={24} color="gray" />
+									<Text style={styles.metadata}>{item.frequency}</Text>
 								</View>
 							</View>
 						</View>
-					}}
-					keyExtractor={todo => todos.id}
+					}
+					keyExtractor={item => item.id}
+				/>
+			</View>
+			<View style={styles.inputSection}>
+				<TextInput
+					style={styles.inputContainer}
+					placeholder={'Ajouter une tache'}
 				/>
 			</View>
 		</SafeAreaView>
@@ -96,9 +114,6 @@ const styles = StyleSheet.create({
 	topSectionRight: {
 		flexDirection: 'row'
 	},
-	bottomSection: {
-		flex: 9
-	},
 	topSectionTitle: {
 		fontSize: 25,
 		textTransform: 'uppercase',
@@ -111,6 +126,41 @@ const styles = StyleSheet.create({
 	},
 	topSectionIcon: {
 		alignSelf: 'center'
+	},
+	bottomSection: {
+		flex: 8,
+		margin: 20
+	},
+	itemContainer: {
+		flexDirection: 'row',
+		marginBottom: 15
+	},
+	itemContainerContent: {
+		marginStart: 10
+	},
+	itemContainerContentMetadata: {
+		flexDirection: 'row'
+	},
+	itemContainerContentTitle: {
+		fontSize: 20,
+		textTransform: 'capitalize',
+		fontWeight: 'bold'
+	},
+	metadata: {
+		fontSize: 17,
+		color: 'gray'
+	},
+	inputSection: {
+		flex: 1,
+		marginBottom: 5,
+		justifyContent: 'center'
+	},
+	inputContainer: {
+		borderWidth: 2,
+		borderColor: 'gray',
+		height: 50,
+		borderRadius: 15,
+		paddingHorizontal: 20
 	}
 });
 
